49b8cc7011ec75d648d245de93f77c56fd1afaa8
isanchez <isanchezz@anl.gov>
2019-05-24 18:54:53 +0000

    Commit

--- example_codes/pi.py
+++ example_codes/pi.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import numpy as np
 from mpi4py import MPI
 
--- submit.sh
+++ submit.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 #COBALT -t 30
-#COBALT -q debug-cache-quad
+#COBALT -q debug
 #COBALT -n 2
-#COBALT -A datascience
+#COBALT -A visualization
 
 # pass container as first argument to script
 CONTAINER=$1
-
-# Use Cray's Application Binary Independent MPI build
-module swap cray-mpich cray-mpich-abi
-
-# include CRAY_LD_LIBRARY_PATH in to the system library path
-export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
-# also need this additional library
-export LD_LIBRARY_PATH=/opt/cray/wlm_detect/1.3.2-6.0.6.0_3.8__g388ccd5.ari/lib64/:$LD_LIBRARY_PATH
-# in order to pass environment variables to a Singularity container create the variable
-# with the SINGULARITYENV_ prefix
-export SINGULARITYENV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-# print to log file for debug
-echo $SINGULARITYENV_LD_LIBRARY_PATH
 
 RANKS_PER_NODE=4
 TOTAL_RANKS=$(( $COBALT_JOBSIZE * $RANKS_PER_NODE ))
@@ -26,4 +13,4 @@ TOTAL_RANKS=$(( $COBALT_JOBSIZE * $RANKS_PER_NODE ))
 # this simply runs the command 'ldd /myapp/pi' inside the container and should show that
 # the app is running agains the host machines Cray libmpi.so not the one inside the container
 # run my contianer like an application, which will run '/myapp/pi'
-aprun -n $TOTAL_RANKS -N $RANKS_PER_NODE singularity run -B /opt:/opt:ro $CONTAINER
+mpirun -n $TOTAL_RANKS -N $RANKS_PER_NODE singularity run -B $CONTAINER
